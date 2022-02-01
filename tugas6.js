84763eb7d721bd132246686feed87ff0bddf1027
zainahda <zainahda@gmail.com>
2022-02-01 18:34:49 +0700

    Niomic tugas 6 Js

---
+++ tugas6.js
@@ -0,0 +1,11 @@
+function ganjilGenap() {
+  for (let i = 1; i <= 20 ; i++){
+    if(i % 2 == 0) {
+      console.log("Bilangan Genap");
+    }else{
+      console.log(i);
+    }
+  }
+}
+
+ganjilGenap();
